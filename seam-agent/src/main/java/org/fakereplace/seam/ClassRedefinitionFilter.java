f9a5595597985636aa2d354b2e79b23c1c6606bb
stuart@baileyroberts.com.au <stuart@baileyroberts.com.au@18b0249e-21b3-11df-9b96-51425d4bd23b>
2010-03-16 09:17:32 +0000

    initial attempt at clearing EL cache
    git-svn-id: https://fakereplace.googlecode.com/svn/trunk@20 18b0249e-21b3-11df-9b96-51425d4bd23b

--- seam-agent/src/main/java/org/fakereplace/seam/ClassRedefinitionFilter.java
+++ seam-agent/src/main/java/org/fakereplace/seam/ClassRedefinitionFilter.java
@@ -5,22 +5,30 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.instrument.ClassDefinition;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
 
+import javax.el.BeanELResolver;
+import javax.el.CompositeELResolver;
+import javax.el.ELResolver;
+import javax.faces.event.PhaseEvent;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Startup;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
@@ -34,193 +42,229 @@ import org.jboss.seam.web.AbstractFilter;
 @Filter
 public class ClassRedefinitionFilter extends AbstractFilter
 {
-    
-    static class FileData
-    {
-	Long lastModified;
-	File file;
-	String className;
-    }
 
-    static final String[] fl = { "seam.properties", "META-INF/components.xml" };
+   static class FileData
+   {
+      Long lastModified;
+      File file;
+      String className;
+   }
 
-    List<FileData> files = null;
+   static final String[] fl = { "seam.properties", "META-INF/components.xml" };
 
-    ReentrantLock lock = new ReentrantLock();
+   List<FileData> files = null;
 
-    Set<File> directories = new HashSet<File>();
+   ReentrantLock lock = new ReentrantLock();
 
-    Method replaceMethod = null;
+   Set<File> directories = new HashSet<File>();
 
-    String AGENT_CLASS = "org.fakereplace.Agent";
+   Method replaceMethod = null;
 
-    boolean enabled = true;
+   String AGENT_CLASS = "org.fakereplace.Agent";
 
-    /**
-     * gets a reference to the replaceClass method. If this fails
-     * because the agent has not been installed then the filter is disabled
-     * If the method suceed then the doInit method is called which scans 
-     * the classpath for exploded jars that can have classes redefined
-     */
-    public ClassRedefinitionFilter()
-    {
-	try
-	{
-	    Class agent = Class.forName(AGENT_CLASS);
-	    replaceMethod = agent.getMethod("redefine", ClassDefinition[].class);
-	    doInit();
+   boolean enabled = true;
 
-	} catch (Exception e)
-	{
-	    System.out.println("------------------------------------------------------------------------");
-	    System.out.println("------ Fakereplace agent not availbile, hot deployment is disabled -----");
-	    System.out.println("------------------------------------------------------------------------");
-	    enabled = false;
-	}
-    }
+   boolean changed = false;
 
-    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
-	    throws IOException, ServletException
-    {
-	if (enabled)
-	{
+   /**
+    * gets a reference to the replaceClass method. If this fails
+    * because the agent has not been installed then the filter is disabled
+    * If the method suceed then the doInit method is called which scans 
+    * the classpath for exploded jars that can have classes redefined
+    */
+   public ClassRedefinitionFilter()
+   {
+      try
+      {
+         Class agent = Class.forName(AGENT_CLASS);
+         replaceMethod = agent.getMethod("redefine", ClassDefinition[].class);
+         doInit();
 
-	    if (lock.tryLock())
-	    {
-		try
-		{
-		    doReplace();
-		} finally
-		{
-		    lock.unlock();
-		}
-	    }
+      } catch (Exception e)
+      {
+         System.out.println("------------------------------------------------------------------------");
+         System.out.println("------ Fakereplace agent not availbile, hot deployment is disabled -----");
+         System.out.println("------------------------------------------------------------------------");
+         enabled = false;
+      }
+   }
 
-	}
-	arg2.doFilter(arg0, arg1);
-    }
+   public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException
+   {
+      if (enabled)
+      {
 
-    void doReplace()
-    {
-	try
-	{
-	    List<ClassDefinition> classesToReplace = new ArrayList<ClassDefinition>();
-	    for (FileData d : files)
-	    {
-		if (d.file.lastModified() > d.lastModified)
-		{
-		    System.out.println("File " + d.className + " has been modified, replacing");
+         if (lock.tryLock())
+         {
+            try
+            {
+               doReplace();
+            } finally
+            {
+               lock.unlock();
+            }
+         }
 
-		    Class ctr = Class.forName(d.className);
-		    byte[] fileData = readFile(d.file);
-		    ClassDefinition cd = new ClassDefinition(ctr, fileData);
-		    classesToReplace.add(cd);
-		    d.lastModified = d.file.lastModified();
+      }
+      arg2.doFilter(arg0, arg1);
+   }
 
-		}
-	    }
-	    if (!classesToReplace.isEmpty())
-	    {
-		ClassDefinition[] data = new ClassDefinition[classesToReplace.size()];
-		for (int i = 0; i < classesToReplace.size(); ++i)
-		{
-		    data[i] = classesToReplace.get(i);
-		}
-		replaceMethod.invoke(null, (Object) data);
-	    }
+   void doReplace()
+   {
+      try
+      {
+         List<ClassDefinition> classesToReplace = new ArrayList<ClassDefinition>();
+         for (FileData d : files)
+         {
+            if (d.file.lastModified() > d.lastModified)
+            {
+               System.out.println("File " + d.className + " has been modified, replacing");
 
-	} catch (Exception e)
-	{
-	    e.printStackTrace();
-	}
-    }
+               Class ctr = Class.forName(d.className);
+               byte[] fileData = readFile(d.file);
+               ClassDefinition cd = new ClassDefinition(ctr, fileData);
+               classesToReplace.add(cd);
+               d.lastModified = d.file.lastModified();
 
-    public void doInit()
-    {
+            }
+         }
+         if (!classesToReplace.isEmpty())
+         {
+            ClassDefinition[] data = new ClassDefinition[classesToReplace.size()];
+            for (int i = 0; i < classesToReplace.size(); ++i)
+            {
+               data[i] = classesToReplace.get(i);
+            }
 
-	try
-	{
-	    for (String resource : fl)
-	    {
-		Enumeration<URL> urls = getClass().getClassLoader().getResources(resource);
+            replaceMethod.invoke(null, (Object) data);
+            changed = true;
 
-		while (urls.hasMoreElements())
-		{
-		    URL i = urls.nextElement();
-		    String path = i.getPath();
-		    path = path.substring(0, path.length() - resource.length() - 1);
-		    File f = new File(path);
-		    if (f.isDirectory() && path.endsWith(".jar"))
-		    {
-			directories.add(f);
-		    }
-		}
+         }
 
-	    }
-	    initialScan();
-	} catch (IOException e)
-	{
-	    throw new RuntimeException(e);
-	}
+      } catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
 
-    }
+   public void doInit()
+   {
 
-    public void initialScan()
-    {
-	files = new ArrayList<FileData>();
-	for (File f : directories)
-	{
-	    handleDirectory(f, f);
-	}
-    }
+      try
+      {
+         for (String resource : fl)
+         {
+            Enumeration<URL> urls = getClass().getClassLoader().getResources(resource);
 
-    private void handleDirectory(File dir, File rootDir)
-    {
-	if (!dir.isDirectory())
-	    return;
-	String dirPath = rootDir.getAbsolutePath();
-	for (File f : dir.listFiles())
-	{
-	    if (!f.isDirectory())
-	    {
-		if (f.getName().endsWith(".class"))
-		{
-		    FileData fd = new FileData();
-		    fd.file = f;
-		    fd.lastModified = f.lastModified();
-		    String d = f.getAbsolutePath().substring(dirPath.length() + 1);
-		    d = d.replace('/', '.');
-		    d = d.replace('\\', '.');
-		    d = d.substring(0, d.length() - ".class".length());
-		    System.out.println(d);
-		    fd.className = d;
-		    files.add(fd);
-		}
-	    } else
-	    {
-		handleDirectory(f, rootDir);
-	    }
-	}
-    }
+            while (urls.hasMoreElements())
+            {
+               URL i = urls.nextElement();
+               String path = i.getPath();
+               path = path.substring(0, path.length() - resource.length() - 1);
+               File f = new File(path);
+               if (f.isDirectory() && path.endsWith(".jar"))
+               {
+                  directories.add(f);
+               }
+            }
 
-    byte[] readFile(File file) throws IOException
-    {
-	InputStream is = new FileInputStream(file);
+         }
+         initialScan();
+      } catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
 
-	long length = file.length();
+   }
 
-	byte[] bytes = new byte[(int) length];
+   @Observer("org.jboss.seam.afterPhase")
+   public void phaseInvalidator(PhaseEvent event)
+   {
+      if (changed)
+      {
+         changed = false;
+         try
+         {
+            javax.faces.context.FacesContext context = (javax.faces.context.FacesContext) Component.getInstance("org.jboss.seam.faces.facesContext");
+            ELResolver resolver = context.getELContext().getELResolver();
+            if (resolver instanceof CompositeELResolver)
+            {
+               CompositeELResolver c = (CompositeELResolver) resolver;
+               Field resolvers = c.getClass().getDeclaredField("resolvers");
+               ELResolver[] resAr = (ELResolver[]) resolvers.get(c);
+               for (ELResolver r : resAr)
+               {
+                  if (r instanceof BeanELResolver)
+                  {
+                     Field cache = r.getClass().getDeclaredField("properties");
+                     Map props = (Map) cache.get(r);
+                     props.clear();
+                  }
+               }
 
-	int offset = 0;
-	int numRead = 0;
-	while (offset < bytes.length
-		&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0)
-	{
-	    offset += numRead;
-	}
+            }
+         } catch (Exception e)
+         {
+            e.printStackTrace();
+         }
+      }
+   }
 
-	is.close();
-	return bytes;
-    }
+   public void initialScan()
+   {
+      files = new ArrayList<FileData>();
+      for (File f : directories)
+      {
+         handleDirectory(f, f);
+      }
+   }
+
+   private void handleDirectory(File dir, File rootDir)
+   {
+      if (!dir.isDirectory())
+         return;
+      String dirPath = rootDir.getAbsolutePath();
+      for (File f : dir.listFiles())
+      {
+         if (!f.isDirectory())
+         {
+            if (f.getName().endsWith(".class"))
+            {
+               FileData fd = new FileData();
+               fd.file = f;
+               fd.lastModified = f.lastModified();
+               String d = f.getAbsolutePath().substring(dirPath.length() + 1);
+               d = d.replace('/', '.');
+               d = d.replace('\\', '.');
+               d = d.substring(0, d.length() - ".class".length());
+               System.out.println(d);
+               fd.className = d;
+               files.add(fd);
+            }
+         } else
+         {
+            handleDirectory(f, rootDir);
+         }
+      }
+   }
+
+   byte[] readFile(File file) throws IOException
+   {
+      InputStream is = new FileInputStream(file);
+
+      long length = file.length();
+
+      byte[] bytes = new byte[(int) length];
+
+      int offset = 0;
+      int numRead = 0;
+      while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0)
+      {
+         offset += numRead;
+      }
+
+      is.close();
+      return bytes;
+   }
 
 }
