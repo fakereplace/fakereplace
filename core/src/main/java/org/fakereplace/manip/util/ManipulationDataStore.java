91e752dc750e6f826109ab2e878c2404c86d8709
stuart@baileyroberts.com.au <stuart@baileyroberts.com.au@18b0249e-21b3-11df-9b96-51425d4bd23b>
2010-06-06 07:04:48 +0000

    removed ClassLoader leak
    git-svn-id: https://fakereplace.googlecode.com/svn/trunk@105 18b0249e-21b3-11df-9b96-51425d4bd23b

--- core/src/main/java/org/fakereplace/manip/util/ManipulationDataStore.java
+++ core/src/main/java/org/fakereplace/manip/util/ManipulationDataStore.java
@@ -6,8 +6,10 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
+
+import com.google.common.base.Function;
+import com.google.common.collect.MapMaker;
 
 /**
  * class that figures out which maniluation should be applied based on the classloader of the relative classes.
@@ -17,20 +19,37 @@ import java.util.concurrent.CopyOnWriteArraySet;
  */
 public class ManipulationDataStore<T extends ClassloaderFiltered<T>>
 {
-   Map<String, Set<T>> data = new ConcurrentHashMap<String, Set<T>>();
+   private final ClassLoader NULL_CLASS_LOADER = new ClassLoader()
+   {
+   };
+
+   Map<ClassLoader, Map<String, Set<T>>> cldata = new MapMaker().weakKeys().makeComputingMap(new Function<ClassLoader, Map<String, Set<T>>>()
+   {
+      public Map<String, Set<T>> apply(ClassLoader from)
+      {
+         return new MapMaker().makeMap();
+      }
+   });
 
    public Map<String, Set<T>> getManipulationDate(ClassLoader loader)
    {
-      Map<String, Set<T>> ret = new HashMap<String, Set<T>>();
-      for (Entry<String, Set<T>> e : data.entrySet())
+      if (loader == null)
       {
-         Set<T> set = new HashSet<T>();
-         ret.put(e.getKey(), set);
-         for (ClassloaderFiltered<T> f : e.getValue())
+         loader = NULL_CLASS_LOADER;
+      }
+      Map<String, Set<T>> ret = new HashMap<String, Set<T>>();
+      for (Entry<ClassLoader, Map<String, Set<T>>> centry : cldata.entrySet())
+      {
+         for (Entry<String, Set<T>> e : centry.getValue().entrySet())
          {
-            if (includeClassLoader(loader, f.getClassLoader()))
+            Set<T> set = new HashSet<T>();
+            ret.put(e.getKey(), set);
+            for (ClassloaderFiltered<T> f : e.getValue())
             {
-               set.add(f.getInstane());
+               if (includeClassLoader(loader, f.getClassLoader()))
+               {
+                  set.add(f.getInstane());
+               }
             }
          }
       }
@@ -40,6 +59,12 @@ public class ManipulationDataStore<T extends ClassloaderFiltered<T>>
 
    public void add(String name, T mdata)
    {
+      ClassLoader loader = mdata.getClassLoader();
+      if (loader == null)
+      {
+         loader = NULL_CLASS_LOADER;
+      }
+      Map<String, Set<T>> data = cldata.get(loader);
       if (!data.containsKey(name))
       {
          data.put(name, new CopyOnWriteArraySet<T>());
@@ -49,7 +74,7 @@ public class ManipulationDataStore<T extends ClassloaderFiltered<T>>
 
    /**
     * even though it is tempting to just try loaderOfClassBeingManipulated.loadClass(manipClassName) if this class
-    * has not been loaded yet then this will cause problems, as this class will not be manipulated. Instead we have 
+    * has not been loaded yet then this will cause problems, as this class will not go through the agent. Instead we have 
     * to try searching through the parent classloaders, which will not always work.
     * 
     *  
@@ -77,6 +102,11 @@ public class ManipulationDataStore<T extends ClassloaderFiltered<T>>
 
    public void remove(String className, ClassLoader classLoader)
    {
+      if (classLoader == null)
+      {
+         classLoader = NULL_CLASS_LOADER;
+      }
+      Map<String, Set<T>> data = cldata.get(classLoader);
       if (data.containsKey(className))
       {
          Set<T> set = data.get(className);
