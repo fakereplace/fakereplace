db2ab2409d3ff5a23c3327c49eff12bc41d92eb6
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-04-10 15:06:01 +1000

    Copyright

--- core/src/main/java/org/fakereplace/manip/FinalMethodManipulator.java
+++ core/src/main/java/org/fakereplace/manip/FinalMethodManipulator.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011, Stuart Douglas
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package org.fakereplace.manip;
 
 import javassist.bytecode.AccessFlag;
@@ -16,7 +35,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 /**
  * manipulator that removes the final attribute from methods
  *
- * @author Stuart Douglas <stuart@baileyroberts.com.au>
+ * @author Stuart Douglas <stuart.w.douglas@gmail.com>
  */
 public class FinalMethodManipulator implements ClassManipulator {
 
